7c5b145a66bbe329df3192e26d8e17ea97df7315
AntonSV <antoxa.svitek@mail.ru>
2022-04-19 11:28:19 +0500

    Create project Java-application

---
+++ Analytics/src/Categories.java
@@ -0,0 +1,32 @@
+
+
+public class Categories {
+    
+        //метод, который добавляет категории
+    public void addCategories(){
+ 	
+ }
+    
+        //метод, который удаляет категории
+    public void deleteCategories(){
+ 	
+ }
+    
+         //метод, который объединяет несколько категорий в одну
+    public void joinCategories(){
+ 	
+ }
+    
+         //метод, который следит за изменением состояния категории
+    // при изменении категории, сообщает об этом классу CooperationExcel, который записывает обновлённые данные в excel
+    public void followCategories(){
+ 	
+ }
+
+        //метод, который отвечает за отображение актуального числа строк в определённой категории
+    public void amountLinesInCategories(){
+ 	
+ }
+    
+    
+}
---
+++ Analytics/src/CooperationExcel.java
@@ -0,0 +1,19 @@
+// import библиотеки Apache POI
+
+
+public class CooperationExcel {
+    
+    
+    //метод, который импортирует данные из файла excel
+    public void importExcel(){
+ 	
+ }
+    
+        //метод, который загружает данные в файл excel
+    public void loadingExcel(){
+ 	
+ }
+    
+    
+    
+}
